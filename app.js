bdfd343b32513246a9ce562f0ed9207faef386f2
Alberto Masia <alberto.masia@gmail.com>
2019-01-31 16:01:26 +0100

    Listen to keydown events and play sound if keyCode is correct.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+(function() {
+
+  window.addEventListener("keydown", playSound);
+
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop the function from running all together
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add("playing");
+  }
+
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return; // skip if it's not a transform
+    this.classList.remove("playing");
+  }
+
+})();
